78a5482a2e16382921c5ee333119d70c5d8a9e84
protolambda <proto@protolambda.com>
2019-01-26 16:03:06 +0100

    update constants

--- lib/beacon/beacon_constants.dart
+++ lib/beacon/beacon_constants.dart
@@ -1,23 +1,61 @@
+/*
+      General
+ */
+const int
 
+    /// 1024 shards.
+    SHARD_COUNT = 1 << 10,
 
-/// 64 slots
-const int EPOCH_LENGTH = 1 << 6;
-/// 6 seconds
-const int SLOT_DURATION = 6;
+    /// = 128 validators
+    TARGET_COMMITTEE_SIZE = 1 << 7,
 
+    /// = 16,000,000,000 Gwei
+    EJECTION_BALANCE = (1 << 3) * 1000000000,
 
-/// the latest 8,192 randao mixes will be tracked.
-const int LATEST_RANDAO_MIXES_LENGTH = 1 << 13;
+    /// = 32
+    MAX_BALANCE_CHURN_QUOTIENT = 1 << 5,
+
+    ///  (= 1,024) votes
+    MAX_CASPER_VOTES = 1 << 10,
+
+    ///  (= 8,192) The latest block roots.
+    LATEST_BLOCK_ROOTS_LENGTH = 1 << 13,
+
+    ///  (= 8,192) The latest 8,192 randao mixes will be tracked.
+    LATEST_RANDAO_MIXES_LENGTH = 1 << 13,
+
+    ///  (= 8,192) The latest 8,192 epochs.
+    LATEST_PENALIZED_EXIT_LENGTH = 1 << 13,
+
+    ///  (= 4)  withdrawals
+    MAX_WITHDRAWALS_PER_EPOCH = 1 << 2;
+
+/*
+      Time
+ */
+const int
+
+    /// 6 seconds
+    SLOT_DURATION = 6,
+
+    /// 4 slots
+    MIN_ATTESTATION_INCLUSION_DELAY = 1 << 2,
+
+    /// 64 slots
+    EPOCH_LENGTH = 1 << 6,
+
+    ///  (= 64)  slots  6.4 minutes
+    SEED_LOOKAHEAD = 1 << 6,
+
+    /// (= 256)  slots  25.6 minutes
+    ENTRY_EXIT_DELAY = 1 << 8,
+
+    ///  (= 1,024)  slots  ~1.7 hours
+    ETH1_DATA_VOTING_PERIOD = 1 << 10,
+
+    ///  (= 16,384)  slots  ~27 hours
+    MIN_VALIDATOR_WITHDRAWAL_TIME = 1 << 14;
 
 /// one vdf output for every epoch.
-const int LATEST_VDF_OUTPUTS_LENGTH = LATEST_RANDAO_MIXES_LENGTH ~/ EPOCH_LENGTH;
-
-/// 1024 shards.
-const int SHARD_COUNT = 1 << 10;
-
-/// the latest 8,192 blocks.
-const int LATEST_BLOCK_ROOTS_LENGTH = 1 << 13;
-
-/// the latest 8,192 epochs.
-const int LATEST_PENALIZED_EXIT_LENGTH = 1 << 13;
-
+const int LATEST_VDF_OUTPUTS_LENGTH =
+    LATEST_RANDAO_MIXES_LENGTH ~/ EPOCH_LENGTH;
